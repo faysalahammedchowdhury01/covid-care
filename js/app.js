d7222e57f343040e37005e99af7e70039a09f90f
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-07-26 18:16:13 +0600

    format

--- js/app.js
+++ js/app.js
@@ -2,24 +2,24 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const navbarItems = document.querySelector('.navbar-items');
 
-let isNavbarOpen = false
+let isNavbarOpen = false;
 
 function toggleNavbar() {
-    if (!isNavbarOpen) {
-        navbarItems.classList.add('open');
-        mobileMenu.innerHTML = `<i class="fas fa-times"></i>`;
-        isNavbarOpen = true;
-    } else {
-        navbarItems.classList.remove('open');
-        mobileMenu.innerHTML = `<i class="fas fa-bars"></i>`;
-        isNavbarOpen = false;
-    }
+  if (!isNavbarOpen) {
+    navbarItems.classList.add('open');
+    mobileMenu.innerHTML = `<i class="fas fa-times"></i>`;
+    isNavbarOpen = true;
+  } else {
+    navbarItems.classList.remove('open');
+    mobileMenu.innerHTML = `<i class="fas fa-bars"></i>`;
+    isNavbarOpen = false;
+  }
 }
 
 mobileMenu.addEventListener('click', toggleNavbar);
 
 // Back to Top
-const backToTopBtn = document.getElementById("back-to-top");
+const backToTopBtn = document.getElementById('back-to-top');
 
 // When the user clicks on the button, scroll to the top of the document
 function backToTop() {
